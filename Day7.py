f409cb0ca8ba13e90e17534dec979872485da627
LongEurope <glennkimyw@gmail.com>
2024-02-13 17:39:46 +1100

    Getting back on the grind

--- Day7.py
+++ Day7.py
@@ -2,8 +2,20 @@
 
 import random
 
-word_list = ['aardvark', 'baboon', 'camel']
+word_list = []
 
-def get_word():
-    global word
-    word = random.choice()
+def choose_word():
+    word = random.choice(word_list)
+
+def user_guess():
+    global guess
+    while True:
+        guess = input('Please input a letter\n')
+        if len(guess) == 1 and guess.isalpha():
+            guess = guess.lower()
+            break
+        else:
+            print('Please input a single letter')
+
+def compare():
+    return
