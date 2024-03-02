c4da8a64db20101c8008b1d29950113cee4448fb
LongEurope <glennkimyw@gmail.com>
2024-03-03 08:15:53 +1100

    Going on day 18

---
+++ Intermediate (15-/Day18/Day18notes.py
@@ -0,0 +1,10 @@
+#Starting to work with graphics and python turtle for GUI and images
+
+#Righclick a variable and press F2 to rename the varaiable everytime it was called
+
+#If you do from library import * it will import everything but is ambigous and should be avoided
+
+#You can do import examplelibrary as l
+#biblioteca = l.library()
+
+#Tuples (1, 2, 3) are python lists that you cannot change the items of. Immutable
---
+++ Intermediate (15-/Day18/main.py
@@ -0,0 +1,7 @@
+#Hirst Painting
+from turtle import Turtle, Screen
+import colorgram
+
+colors = colorgram.extract('Hirst.jpg', 10)
+
+print(colors)
