0edd5f6b8376052fab03fd1d5d05834c0a3d5c06
LongEurope <glennkimyw@gmail.com>
2024-03-05 19:38:39 +1100

    Fixed day 18 colorgram error

--- Beginner (1-14)/Day9.py
+++ Beginner (1-14)/Day9.py
@@ -12,16 +12,17 @@ winner = {'name': None, 'bid': 0}
 os.system('cls')
 print('Welcome to blind bidder')
 while True:
+    os.system('cls')
     name = input('Input the name you wish to be attached for your bid: ')
     bid = int(input('Input the amount of money you wish to bid: '))
     participants[name] = bid
     keep = input('Would you like to add another participant? Enter "y" to add, enter any other key to finish')
     if keep != 'y':
-        os.system('cls')
         break
 for person in participants:
     if participants[person] > winner['bid']:
         winner['name'] = person
         winner['bid'] = participants[person]
 
+os.system('cls')
 print(f'The winner of the auction is {winner["name"]} bidding at ${winner["bid"]}')
--- Intermediate (15-/Day18/main.py
+++ Intermediate (15-/Day18/main.py
@@ -2,6 +2,6 @@
 from turtle import Turtle, Screen
 import colorgram
 
-colors = colorgram.extract('Hirst.jpg', 10)
+colors = colorgram.extract(r"C:\Users\glenn\OneDrive\Documents\Python\DOC100\Intermediate (15-\Day18\hirst.png", 10)
 
 print(colors)
